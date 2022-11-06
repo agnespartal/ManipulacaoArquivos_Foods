152c3d875b553e4b85b8977ab0a78148cbc054b3
Agnes <95083660+agnespartal@users.noreply.github.com>
2022-11-06 19:31:58 -0300

    rev1

--- src/model/FoodsController.java
+++ src/model/FoodsController.java
@@ -12,36 +12,22 @@ public class FoodsController {
 		super();
 	}
 
-	public void fruitsController(String caminho, String arquivo) throws Exception{
-		File arq = new File(caminho,arquivo);
+	public void fruitsController(String caminho, String arquivo) throws Exception {
+		File arq = new File(caminho, arquivo);
 		if (arq.exists() && arq.isFile()) {
 			FileInputStream fluxo = new FileInputStream(arq);
 			InputStreamReader leFluxo = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leFluxo);
 			String linha = buffer.readLine();
-			System.out.println("FOOD NAME \t\t SCIENTFIC NAME \t\t SUBGROUP\n");
+			System.out.format("%-30s\t" + "%-50s\t" + "%-30s\n", "FOOD NAME", "SCIENTFIC NAME", "SUBGROUP");
 			while (linha != null) {
 				if (linha.contains(",")) {
 					String[] vetLinha = linha.split(",");
 					if (vetLinha[2].contains("Fruits")) {
-						if(vetLinha[0].length() > 7 && vetLinha[0].length() <= 12) {
-							System.out.print(vetLinha[0] + "\t\t");
-						}else if(vetLinha[0].length() > 12) {
-							System.out.print(vetLinha[0] + " \t");
-						}else {
-							System.out.print(vetLinha[0] + "\t\t\t");
-						}
-						if (vetLinha[1].length() > 14 && vetLinha[1].length() <= 20) {
-							System.out.print(vetLinha[1] + "\t\t");
-						}else if (vetLinha[1].length() > 20){
-							System.out.print(vetLinha[1] + " \t");
-						}else {
-							System.out.print(vetLinha[1] + "\t\t\t");
-						}
-						System.out.println(vetLinha[3]);
-						}
+						System.out.format("%-30s\t" + "%-50s\t" + "%-30s\n", vetLinha[0], vetLinha[1], vetLinha[3]);
+					}
+					linha = buffer.readLine();
 				}
-				linha = buffer.readLine();
 			}
 			buffer.close();
 			leFluxo.close();
